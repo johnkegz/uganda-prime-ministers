c4cbeb7f40b056e57afcd5a926361cbb71f49e81
johnkegz <johnkal24@gmail.com>
2020-02-17 10:12:21 +0300

    Test get prime minister functionality

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require("assert");
+var chai = require("chai");
+var primeMinisters = require("../index");
+
+describe("Testing for get prime ministers functionality", function() {
+  it("user should get all uganda prime ministers", function() {
+    const f = primeMinisters.getPrimeMinisters();
+    chai.assert.typeOf(primeMinisters.getPrimeMinisters(), "Array");
+  });
+  it(`user should get prime minister's bithDay`, function() {
+    assert.equal(
+      primeMinisters.getPrimeMinisterBirthDay("Milton Obote").birthDay,
+      "28 December 1925"
+    );
+  });
+});
